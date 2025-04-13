6e61064bf5e6eef52b51d46b363d7a5a573fafd7
nekusu <nekusu.dev@gmail.com>
2025-04-13 17:03:11 -0300

    Initialize accreditations service with Spring Boot

---
+++ src/main/java/com/sharks/accreditations_service/AccreditationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sharks.accreditations_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccreditationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccreditationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharks/accreditations_service/AccreditationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharks.accreditations_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccreditationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
